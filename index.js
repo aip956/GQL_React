c2f5c58097bf83c481e52f23a47af9f85c5e4498
Anthea Ip <antheaip964@gmail.com>
2022-02-13 22:50:38 -0500

    Working

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ require("dotenv").config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
-const {gql} = require('apollo-server-express');
-// const { MONGODB_URL } = process.env;
-const URL = "mongodb+srv://sei:seirocks@cluster0.acqm3.mongodb.net/GQL_React?retryWrites=true&w=majority"
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
 
 
+const { URL } = process.env;
+
+
+//  Database connection
 mongoose.connect(
     URL,
     { 
@@ -17,28 +20,15 @@ mongoose.connect(
      () => console.log("DB Connected")
      );
 
-// Queries
-const typeDefs = gql`
-type Query {
-hello: String
-}
-`;
 
-// Resolvers
-const resolvers = {
-    Query: {
-        hello: () => {
-            return "Hello World";
-        },
-    },
-};
 
 const startServer=async()=>{
-    const app=express()
-    const ApolloServer=new ApolloServer({
-        typeDefs,resolvers
+    const app = express()
+    const apolloServer = new ApolloServer({
+        typeDefs,resolvers,
     })
-    await ApolloServer.start()
-    ApolloServer.applyMiddleware({app:app});
+    await apolloServer.start()
+    apolloServer.applyMiddleware({app:app});
     app.listen(4000,()=>console.log("Server up and running on 4000"));
 }
+startServer(); 
