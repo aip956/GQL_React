8c0572b50fb617075c3823ab2285e37fe358fc6b
Anthea Ip <antheaip964@gmail.com>
2022-02-14 18:00:52 -0500

    Working CRUD

--- resolvers.js
+++ resolvers.js
@@ -16,6 +16,21 @@ const resolvers = {
         const post=await new Post({title,description}).save()
         return post
         },
+        updatePost: async(parent,args,context,info) => {
+            const{id}=args;
+            const {title,description}=args.post;
+            const post= await Post.findByIdAndUpdate(
+                id,
+                {title,description},
+                {new:true}
+                );
+            return post;
+        },
+        deletePost:async(parent,args,context,info)=>{
+            const {id}=args
+            await Post.findByIdAndDelete(id)
+            return "Deleted";
+        }    
     },
 };
 
--- typeDefs.js
+++ typeDefs.js
@@ -19,6 +19,8 @@ description:String
 }
 type Mutation {
 createPost(post:PostInput):Post
+updatePost(id:String,post:PostInput):Post
+deletePost(id:String):String
 }
 `;
 
